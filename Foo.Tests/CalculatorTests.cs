e6ffee4d542b8ab67d6f0cdc0c602848f9ce05ae
Namit <namit2saxena@gmail.com>
2018-03-17 20:19:40 +0530

    Added a string evaluating function.

--- Foo.Tests/CalculatorTests.cs
+++ Foo.Tests/CalculatorTests.cs
@@ -13,5 +13,17 @@ namespace Foo.Tests
         {
             Assert.AreEqual(3, Calculator.Add(1, 2));
         }
+        [Test]
+        public void EvalOnePlusTwo()
+        {
+            Assert.AreEqual(3, Calculator.Eval("1 + 2"));
+        }
+
+        [TestCase("1 + 2", 3)]
+        [TestCase("1 - 2", -1)]
+        public void EvalOnePlusTwo(string text, int expected)
+        {
+            Assert.AreEqual(expected, Calculator.Eval(text));
+        }
     }
 }
--- Foo/Calculator.cs
+++ Foo/Calculator.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 namespace Foo
 {
     public static class Calculator
     {
         public static int Add(int x, int y) => x + y;
+        public static int Eval(string inputText) => (int)new DataTable().Compute(inputText, null);
+
+        public static void ParsesString(string inputText)
+        {
+            foreach (var match in Regex.Matches(inputText, @"([*+/\-)(])|([0-9]+)"))
+            {
+                Console.WriteLine(match.ToString());
+            }
+        }
+
     }
 }
