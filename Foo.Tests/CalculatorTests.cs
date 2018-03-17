c442a9facde933f5361db922d49c7837a373584e
Namit <namit2saxena@gmail.com>
2018-03-17 13:16:46 +0530

    Added Tests with Packages

---
+++ Foo.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Foo.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void OnePlusTwo()
+        {
+            Assert.AreEqual(3, Calculator.Add(1, 2));
+        }
+    }
+}
--- Foo/Calculator.cs
+++ Foo/Calculator.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Foo
 {
-    public class Class1
+    public static class Calculator
     {
+        public static int Add(int x, int y) => x + y;
     }
 }
